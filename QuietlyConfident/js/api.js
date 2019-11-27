104b759fa392804091b7782e6ad77ed985e2dbcf
Addrick <asantos@wpi.edu>
2019-11-27 02:19:20 -0500

    edited html, javascript to use our api calls, aws should work but giving server 500 errors

---
+++ QuietlyConfident/js/api.js
@@ -0,0 +1,9 @@
+// all access driven through BASE. Must end with a SLASH
+// be sure you change to accommodate your specific API Gateway entry point
+var base_url = "https://kv30lpjbbe.execute-api.us-east-2.amazonaws.com/somethingepic/"; 
+
+var add_url    = base_url + "calculator";   // POST
+var create_url = base_url + "constants";    // POST
+var delete_url = base_url + "constant";     // POST because of CORS
+var list_pl   = base_url + "list_playlists";    // GET
+var list_vid   = base_url + "list_video_segments";    // GET
