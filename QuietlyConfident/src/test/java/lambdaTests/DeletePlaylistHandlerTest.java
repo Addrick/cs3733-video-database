343d4664a5fa1c04bb465bee9f2b780ca1a437cf
addrick <asantos@wpi.edu>
2019-12-04 17:12:45 -0500

    made DeletePlaylistHandlerTest compile. it probably does nothing useful i just wanted the angry red X icons to go away in eclipse

--- QuietlyConfident/src/test/java/lambdaTests/DeletePlaylistHandlerTest.java
+++ QuietlyConfident/src/test/java/lambdaTests/DeletePlaylistHandlerTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import http.*;
+import lambda.CreatePlaylistHandler;
+import lambda.DeletePlaylistHandler;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
@@ -16,7 +18,7 @@ public class DeletePlaylistHandlerTest extends LambdaTest {
     public void testCreateAndDeletePlaylist() {
     	// create playlist
         int rnd = (int) (Math.random() * 1000000);
-        CreatePlaylistRequest ccr = new CreatePlaylistRequest("x" + rnd, "Mi43MTgyODE4Mjg=");
+        CreatePlaylistRequest ccr = new CreatePlaylistRequest("x" + rnd);
         
         CreatePlaylistResponse resp = new CreatePlaylistHandler().handleRequest(ccr, createContext("create"));
         Assert.assertEquals("x" + rnd, resp.response);
@@ -24,7 +26,7 @@ public class DeletePlaylistHandlerTest extends LambdaTest {
         // now delete
         DeletePlaylistRequest dcr = new DeletePlaylistRequest("x" + rnd);
         DeletePlaylistResponse d_resp = new DeletePlaylistHandler().handleRequest(dcr, createContext("delete"));
-        Assert.assertEquals("x" + rnd, d_resp.name);
+        Assert.assertEquals("x" + rnd, d_resp.id_playlist);
         
         // try again and this should fail
         d_resp = new DeletePlaylistHandler().handleRequest(dcr, createContext("delete"));
