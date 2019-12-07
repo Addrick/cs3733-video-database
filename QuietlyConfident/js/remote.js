0c92e89e452f17401f71d1d331057601b7381454
Addrick <asantos@wpi.edu>
2019-12-07 17:35:12 -0500

    fixed some stuff on html, started some on the remote handlers

---
+++ QuietlyConfident/js/remote.js
@@ -0,0 +1,86 @@
+function refreshRemoteList() {
+	
+  var data = {};
+  data["minValue"] = 1;
+  data["maxValue"] = 3;
+  var js = JSON.stringify(data);	
+	
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", search_remote_url, true);
+  xhr.setRequestHeader("x-api-key", apikey);
+
+  // send the collected data as JSON
+  xhr.send(js);
+   
+  console.log("sent");
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log ("XHR:" + xhr.responseText);
+      processRemoteListResponse(xhr.responseText);
+    } else {
+    	processRemoteListResponse("N/A");
+    }
+  };
+  
+  // ALSO SEND remote video segments
+  var another = new XMLHttpRequest();
+  another.open("GET", sample_video_segments_url, true);
+  another.setRequestHeader("x-api-key", apikey);
+
+  // send the collected data as JSON
+  another.send();
+   
+  console.log("sent");
+
+  // This will process results and update HTML as appropriate. 
+  another.onloadend = function () {
+    if (another.readyState == XMLHttpRequest.DONE) {
+      console.log ("XHR:" + another.responseText);
+      processRemoteVideoResponse(another.responseText);
+    }
+  };
+}
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+function processRemoteVideoResponse(result) {
+  console.log("res-vs:" + result);
+  
+  var remoteVideoSegmentList = document.getElementById('remoteVideoSegmentsList');
+  remoteVideoSegmentList.innerHTML = "<code>" + result + "</code><p>";
+}
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+function processRemoteListResponse(result) {
+  console.log("res:" + result);
+  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+  var js = JSON.parse(result);
+  var remoteConstList = document.getElementById('remoteConstantList');
+  
+  var output = "";
+  for (var i = 0; i < js.list.length; i++) {
+    var constantJson = js.list[i];
+    console.log(constantJson);
+    
+    var cname = constantJson["name"];
+    var cval = constantJson["value"];
+    var sysvar = constantJson["system"];
+    if (sysvar) {
+    	output = output + "<div id=\"const" + cname + "\"><b>" + cname + ":</b> = " + cval + "<br></div>";
+    } else {
+    	output = output + "<div id=\"const" + cname + "\"><b>" + cname + ":</b> = " + cval + "(<a href='javaScript:requestDelete(\"" + cname + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
+    }
+  }
+
+  // Update computation result
+  remoteConstList.innerHTML = output;
+}
