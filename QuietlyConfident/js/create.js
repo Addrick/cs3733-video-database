dad78df2b7b746a8c816a32ae3b8f18318792368
bjon125 <bjonathon125@gmail.com>
2019-12-07 18:13:11 -0500

    fixed my screwup

--- QuietlyConfident/js/create.js
+++ QuietlyConfident/js/create.js
@@ -33,44 +33,6 @@ function handleCreateClick(e)
 				console.log ("XHR:" + xhr.responseText);
 				alert("XHR:" + xhr.responseText);
 				processCreateResponse(xhr.responseText);
-=======
-function AddPlaylist()
-{
-	var txt;
-	var pl = prompt("Please enter the playlist name:", "New Playlist");
-	if (pl == null || pl == "")
-	{}
-	else
-	{
-		txt = "Created playlist: " + pl;
-		var data = {};
-		data["id_playlist"] = pl;
-		alert(txt);
-	
-		var js = JSON.stringify(data);
-		console.log("JS:" + js);
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", create_pl_url, true);
-	
-		// send the collected data as JSON
-		xhr.send(js);
-	
-		// This will process results and update HTML as appropriate. 
-		xhr.onloadend = function () {
-			console.log(xhr);
-			console.log(xhr.request);
-			if (xhr.readyState == XMLHttpRequest.DONE) {
-				if (xhr.status == 200) {
-					console.log ("XHR:" + xhr.responseText);
-					alert("XHR:" + xhr.responseText);
-					processCreateResponse(xhr.responseText);
-				} else {
-					console.log("actual:" + xhr.responseText)
-					var js = JSON.parse(xhr.responseText);
-					var err = js["response"];
-					alert (err);
-				}
->>>>>>> branch 'bettermaybe' of https://github.com/Addrick/cs3733-video-database.git
 			} else {
 				processCreateResponse("N/A");
 			}
