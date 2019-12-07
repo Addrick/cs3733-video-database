6b9e839eb781114d38e5ce94e0be70d3c21242c3
bjon125 <bjonathon125@gmail.com>
2019-12-07 17:57:06 -0500

    The Add Playlist button on the site works now

--- QuietlyConfident/js/create.js
+++ QuietlyConfident/js/create.js
@@ -1,46 +1,50 @@
-function processCreateResponse(result) {
+function processCreateResponse(result)
+{
 	// Can grab any DIV or SPAN HTML element and can then manipulate its
 	// contents dynamically via javascript
 	console.log("result:" + result);
-
 	refreshPlaylistList();
-
 }
 
-function handleCreateClick(e) {
-	var form = document.createForm;
-
-	var data = {};
-	data["id_playlist"] = createPlaylistForm.id_playlist.value;
-
-	alert(data);
-
-	var js = JSON.stringify(data);
-	console.log("JS:" + js);
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", create_pl_url, true);
-
-	// send the collected data as JSON
-	xhr.send(js);
-
-
-	// This will process results and update HTML as appropriate. 
-	xhr.onloadend = function () {
-		console.log(xhr);
-		console.log(xhr.request);
-		if (xhr.readyState == XMLHttpRequest.DONE) {
-			if (xhr.status == 200) {
-				console.log ("XHR:" + xhr.responseText);
-				alert("XHR:" + xhr.responseText);
-				processCreateResponse(xhr.responseText);
+function AddPlaylist()
+{
+	var txt;
+	var pl = prompt("Please enter the playlist name:", "New Playlist");
+	if (pl == null || pl == "")
+	{}
+	else
+	{
+		txt = "Created playlist: " + pl;
+		var data = {};
+		data["id_playlist"] = pl;
+		alert(txt);
+	
+		var js = JSON.stringify(data);
+		console.log("JS:" + js);
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", create_pl_url, true);
+	
+		// send the collected data as JSON
+		xhr.send(js);
+	
+		// This will process results and update HTML as appropriate. 
+		xhr.onloadend = function () {
+			console.log(xhr);
+			console.log(xhr.request);
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				if (xhr.status == 200) {
+					console.log ("XHR:" + xhr.responseText);
+					alert("XHR:" + xhr.responseText);
+					processCreateResponse(xhr.responseText);
+				} else {
+					console.log("actual:" + xhr.responseText)
+					var js = JSON.parse(xhr.responseText);
+					var err = js["response"];
+					alert (err);
+				}
 			} else {
-				console.log("actual:" + xhr.responseText)
-				var js = JSON.parse(xhr.responseText);
-				var err = js["response"];
-				alert (err);
+				processCreateResponse("N/A");
 			}
-		} else {
-			processCreateResponse("N/A");
-		}
-	};
+		};
+	}
 }
